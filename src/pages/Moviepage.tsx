05ad6f1baf07db126308e31689cce60e4d7536ef
Kalpak43 <111725804+Kalpak43@users.noreply.github.com>
2025-02-20 15:33:40 +0530

    created movie page

--- src/pages/Moviepage.tsx
+++ src/pages/Moviepage.tsx
@@ -10,15 +10,9 @@ import { Star } from "lucide-react";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
+import { FaImdb } from "react-icons/fa6";
+import { SiMetacritic, SiRottentomatoes } from "react-icons/si";
 
 const API_KEY = import.meta.env.VITE_OMDB_KEY;
 const BASE_URL = import.meta.env.VITE_OMDB_URL;
@@ -42,6 +36,8 @@ function MoviePage() {
         );
         setMovie({ ...movieData, fav: isFavorite });
         setLoading(false);
+
+        console.log(movieData);
       })
       .catch((e) => {
         console.error(e);
@@ -91,7 +87,7 @@ function MoviePage() {
     <div className="space-y-2">
       <div className="space-y-2">
         <h1 className="text-3xl font-[500]">{movie.Title}</h1>
-        <div className="flex items-center justify-between py-1">
+        <div className="flex max-md:flex-col  md:items-center justify-between py-1">
           <div className="flex items-center gap-2">
             <p className="text-sm text-muted-foreground pr-2">{movie.Year}</p>
             <div className="p-[2px] bg-muted-foreground rounded-full"></div>
@@ -106,7 +102,7 @@ function MoviePage() {
                 ? removeFromFavorites(movie)
                 : handleAddToFavorties(movie);
             }}
-            className={`${
+            className={`max-md:hidden ${
               movie.fav
                 ? "bg-[#f6c700]"
                 : "border-1 border-[#f6c700] bg-transparent text-primary hover:bg-[#f6c700]"
@@ -119,15 +115,15 @@ function MoviePage() {
       </div>
 
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
-        <Card>
+        <Card className="w-full">
           <img
             src={movie.Poster}
             alt={movie.Title}
             className="w-full rounded-lg shadow-md"
           />
         </Card>
-        <div className="col-span-3 h-full">
-          <Card className="h-full p-4">
+        <div className="md:col-span-3 h-full">
+          <Card className="h-full p-4 flex flex-col gap-4">
             <Table>
               <TableBody>
                 <TableRow>
@@ -135,7 +131,11 @@ function MoviePage() {
                   <TableCell>
                     <div className="flex gap-2 flex-wrap">
                       {movie.Genre.split(",").map((g) => (
-                        <Badge className="py-1 px-4" variant={"secondary"}>
+                        <Badge
+                          key={g}
+                          className="py-1 px-4"
+                          variant={"secondary"}
+                        >
                           {g}
                         </Badge>
                       ))}
@@ -156,6 +156,24 @@ function MoviePage() {
                 </TableRow>
               </TableBody>
             </Table>
+            <div className="flex max-md:flex-col justify-center items-center gap-8 flex-wrap flex-1">
+              {movie.Ratings.map((rating) => {
+                return (
+                  <div key={rating.Source} className="text-center">
+                    {rating.Source == "Internet Movie Database" && (
+                      <FaImdb className="text-[#e2b616]" size={100} />
+                    )}
+                    {rating.Source == "Rotten Tomatoes" && (
+                      <SiRottentomatoes className="text-[#fa320a]" size={100} />
+                    )}
+                    {rating.Source == "Metacritic" && (
+                      <SiMetacritic className="text-[#00ce7a]" size={100} />
+                    )}
+                    <p className="text-[var(--highlight)]">{rating.Value}</p>
+                  </div>
+                );
+              })}
+            </div>
           </Card>
         </div>
       </div>
