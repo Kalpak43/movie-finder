d5f52a0c648ba6eddb4fe4c26138c2efc144a1e2
Kalpak43 <111725804+Kalpak43@users.noreply.github.com>
2025-02-21 13:50:52 +0530

    updated search page

--- src/pages/Searchpage.tsx
+++ src/pages/Searchpage.tsx
@@ -62,7 +62,9 @@ function Searchpage() {
 
       {status === "succeeded" && moviesWithFav.length > 0 && (
         <>
-          <h1 className="text-2xl font-bold">Search Results: </h1>
+          <h1 className="text-2xl font-bold">
+            Showing search results for: <span className="text-[var(--highlight)]">{query}</span>{" "}
+          </h1>
           <ScrollArea className="w-full whitespace-nowrap overflow-x-auto py-2">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4">
               {moviesWithFav.map((movie) => (
